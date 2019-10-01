54769862fa3df03abfdd0c5864babfebbe7d9de6
kevindhanna <kevindhanna@gmail.com>
2019-10-01 16:24:56 +0100

    add player name input

--- app.rb
+++ app.rb
@@ -1,7 +1,17 @@
 require 'sinatra'
 
 class Battle < Sinatra::Base
+  get '/test' do
+    erb :infrastructure_test
+  end
+  
   get '/' do
-    "Testing infrastructure working!"
+    erb :players
+  end
+
+  post '/menu' do
+    @player1 = params[:player1]
+    @player2 = params[:player2]
+    erb :menu
   end
 end
--- spec/features/battle_feature_spec.rb
+++ spec/features/battle_feature_spec.rb
@@ -1,10 +1,19 @@
-# require 'spec_helper'
-
 feature 'Testing infrastructure' do
-
   scenario "should display the Homepage" do
-    visit("/")
+    visit("/test")
     expect(page).to have_content('Testing infrastructure working!')
   end
+end
 
+feature 'entering names' do
+  scenario 'should dispaly the player names form' do
+    visit('/')
+    name1 = "Kevin"
+    name2 = "Nabille"
+    fill_in "player1", with: name1
+    fill_in "player2", with: name2
+    click_button("Battle!")
+    expect(page).to have_content("Player 1: #{name1}")
+    expect(page).to have_content("Player 2: #{name2}")
+  end
 end
